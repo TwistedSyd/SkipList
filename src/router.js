d127acb435ce49717f4e16b6f1793cf3151114f4
Sydney Koncz <SKoncz@jeldwen.com>
2018-09-07 10:46:18 -0700

    Add Skip and Delete Skip working

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@
    use for routing purposes */ 
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import Dashboard from './views/Dashboard.vue'
 
@@ -12,11 +11,6 @@ export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
-    {
-        path: '/home',
-        name: 'home',
-        component: Home
-    },
     {
         path: "/",
         name: 'login',
