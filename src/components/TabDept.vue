2e557baba1a84d163a0eb44aa3525a3673e3ceb8
Sydney Koncz <SKoncz@jeldwen.com>
2018-09-07 05:59:03 -0700

    Added TabDept

---
+++ src/components/TabDept.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{ $props.category}}
+    </div>
+</template>
+
+<script>
+import { db } from './main'
+
+export default {
+    name: 'TabDept',
+    props: {
+        category: String
+    },
+    data() {
+        return {
+            skips: []
+        }
+    },
+    firestore() {
+        return {
+            skips: db.collection('categories').doc(this.$props.category).collection('skips')
+        }
+    }
+}
+</script>
+
